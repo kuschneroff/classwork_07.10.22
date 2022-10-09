e83cb4ae7bbdbb124b2d2b512a86aa89ccc69b1f
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-09 17:21:49 +0300

    задача 3

---
+++ Task#3/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 21: Напишите программу, 
+которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+
+AB = √(xb - xa)2 + (yb - ya)2
+
+*/
+
+void GetDistanceBetweenTwoPoints(int firstX, int secondX, int firstY, int secondY)
+{
+    double result = 0;
+
+    int tempX = (secondX - firstX) * (secondX - firstX);
+    int tempY = (secondY - firstY) * (secondY - firstY);
+
+    result = Math.Sqrt(tempX + tempY);
+    
+    Console.WriteLine($"расстояние между двумя точками = {result}");
+}
+
+
+/*Второй Варинт решения этой же задачи
+Point GetPoint()
+{
+    Point point = new Point();
+    Console.WriteLine("Введите координату Х:");
+    point.X = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату У:");
+    point.Y = int.Parse(Console.ReadLine());
+
+    return point;
+}
+
+
+Point firstPoint = GetPoint();
+Point secondPoint = GetPoint();
+
+double result = Math.Sqrt(Math.Pow(firstPoint.X - secondPoint.X, 2) + Math.Pow(firstPoint.Y - secondPoint.Y,2));
+Console.WriteLine($"")
+class Point
+{
+    public int X { get; set;}
+    public int Y { get; set;}
+}
+*/
